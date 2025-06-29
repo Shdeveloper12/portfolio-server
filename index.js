0e8fc327276d3860b756193170dcdf82f34ee7f0
Shdevloper <shafayet237@gmail.com>
2025-06-29 16:47:37 +0600

    added post request to client side

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.port || 3000;
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.dgti16b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const collection = client.db("portfolio").collection("contact");
+
+
+
+    // Create a document to insert
+    app.post("/contact", async (req, res) => {
+      const data = req.body;
+      const result = await collection.insertOne(data);
+      
+
+      res.json(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Portfolio Running");
+});
+
+app.listen(port, () => {
+  console.log(`Portfolio server is running on port ${port}`);
+});
